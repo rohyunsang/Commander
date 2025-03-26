24839e90da0c96b390bb3ce4a7691e7a37c6467b
Roh <82229769+rohyunsang@users.noreply.github.com>
2025-03-26 20:31:20 +0900

    Initial commit

---
+++ Source/Commander/CommanderGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CommanderGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACommanderGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACommanderGameMode();
+};
+
+
+
